0497cb7954295b75ce577f51591f11484bd978c5
EmmanuelHmar <eman.hmar@gmail.com>
2018-03-16 16:58:23 -0400

    files from master

---
+++ README.md
@@ -0,0 +1,19 @@
+# Stellar
+
+Create a student registration system. Custom GoSolar. 
+
+Team members: Glenn, Amber, Gabby, Hamed, E
+
+Course: CSc 4350
+
+What is Stellar:
+
+Stellar is an application that helps students manage their classes in a more efficient way. 
+
+1)Students shall be able to search and register for the classes they need (CRNs, class title, and description). 
+
+2)Because of the rapid rate in which classes fill and classes being added into the system at different times, students shall also have an alert system to send a notification when a class has been opened or warn them when a class is almost at max capacity (when they’re about 5 seats left). 
+
+3)The software shall also provide feedback to the students on their academic progression, displaying their GPA (4.0 scale), current classes, previous classes taken and prerequisite each class.  
+
+
---
+++ Stellar.java
@@ -0,0 +1,39 @@
+package stellar;
+
+ public class Stellar {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+     //The main method calls the Login class and makes it visible. It also gives the window a "Nimbus look". The Nimbus look is optional.
+     public static void main(String[] args) {
+        Login log = new Login();
+        //For the Nimbus Look
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Stellar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Stellar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Stellar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Stellar.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        
+        //Creates and displays the windows
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+             }
+        });
+    }
+    
+}
