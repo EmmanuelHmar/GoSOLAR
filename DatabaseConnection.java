d49dd99c1f63832450ebdb051a9a4d046a28bb14
hamedouldhamou <33500154+hamedouldhamou@users.noreply.github.com>
2018-04-23 09:58:03 -0400

    commented

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,15 +5,15 @@ import javax.swing.*;
 
 public class DatabaseConnection {
 	Connection conn=null;
+	/* This method connect the database to the code */
 public static Connection dbConnector()
 {
     
    
 	try {
 		Class.forName("org.sqlite.JDBC");
-		Connection conn=DriverManager.getConnection("jdbc:sqlite:/Users/hamedouldhamou/Desktop/Hamed/Stellar/DataBase.db");
-		//JOptionPane.showMessageDialog(null, "Connection Successful");
-		return conn;
+		Connection conn=DriverManager.getConnection("jdbc:sqlite:/Users/hamedouldhamou/Desktop/Hamed/Stellar/DataBase.db"); // Connecting the DataBase by using the path
+		return conn; // the method returns the connection
 	}
 	catch(Exception e)
 	{
